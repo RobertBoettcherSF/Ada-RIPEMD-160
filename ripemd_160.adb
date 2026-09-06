7473234553d6e2aad8333fd0bdecff94a3b223d3
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-09-06 23:42:43 +0200

    Refactor RIPEMD-160 constants to use array syntax

--- ripemd_160.adb
+++ ripemd_160.adb
@@ -6,51 +6,51 @@ package body Ripemd_160 is
 
    -- RIPEMD-160 Constants for the Left line (KL)
    KL : constant array (Step_Index) of Word :=
-     ( 0 .. 15 => 16#00000000#,
+     [ 0 .. 15 => 16#00000000#,
       16 .. 31 => 16#5A827999#,
       32 .. 47 => 16#6ED9EBA1#,
       48 .. 63 => 16#8F1BBCDC#,
-      64 .. 79 => 16#A953FD4E#);
+      64 .. 79 => 16#A953FD4E#];
 
    -- RIPEMD-160 Constants for the Right line (KR)
    KR : constant array (Step_Index) of Word :=
-     ( 0 .. 15 => 16#50A28BE6#,
+     [ 0 .. 15 => 16#50A28BE6#,
       16 .. 31 => 16#5C4DD124#,
       32 .. 47 => 16#6D703EF3#,
       48 .. 63 => 16#7A6D76E9#,
-      64 .. 79 => 16#00000000#);
+      64 .. 79 => 16#00000000#];
 
    -- Message Word Permutations for Left line (RL)
    RL : constant array (Step_Index) of Word_Index :=
-     ( 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
+     [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
        7,  4, 13,  1, 10,  6, 15,  3, 12,  0,  9,  5,  2, 14, 11,  8,
        3, 10, 14,  4,  9, 15,  8,  1,  2,  7,  0,  6, 13, 11,  5, 12,
        1,  9, 11, 10,  0,  8, 12,  4, 13,  3,  7, 15, 14,  5,  6,  2,
-       4,  0,  5,  9,  7, 12,  2, 10, 14,  1,  3,  8, 11,  6, 15, 13);
+       4,  0,  5,  9,  7, 12,  2, 10, 14,  1,  3,  8, 11,  6, 15, 13];
 
    -- Message Word Permutations for Right line (RR)
    RR : constant array (Step_Index) of Word_Index :=
-     ( 5, 14,  7,  0,  9,  2, 11,  4, 13,  6, 15,  8,  1, 10,  3, 12,
+     [ 5, 14,  7,  0,  9,  2, 11,  4, 13,  6, 15,  8,  1, 10,  3, 12,
        6, 11,  3,  7,  0, 13,  5, 10, 14, 15,  8, 12,  4,  9,  1,  2,
       15,  5,  1,  3,  7, 14,  6,  9, 11,  8, 12,  2, 10,  0,  4, 13,
        8,  6,  4,  1,  3, 11, 15,  0,  5, 12,  2, 13,  9,  7, 10, 14,
-      12, 15, 10,  4,  1,  5,  8,  7,  6,  2, 13, 14,  0,  3,  9, 11);
+      12, 15, 10,  4,  1,  5,  8,  7,  6,  2, 13, 14,  0,  3,  9, 11];
 
    -- Rotate Amounts for Left line (SL)
    SL : constant array (Step_Index) of Shift_Amount :=
-     (11, 14, 15, 12,  5,  8,  7,  9, 11, 13, 14, 15,  6,  7,  9,  8,
+     [11, 14, 15, 12,  5,  8,  7,  9, 11, 13, 14, 15,  6,  7,  9,  8,
        7,  6,  8, 13, 11,  9,  7, 15,  7, 12, 15,  9, 11,  7, 13, 12,
       11, 13,  6,  7, 14,  9, 13, 15, 14,  8, 13,  6,  5, 12,  7,  5,
       11, 12, 14, 15, 14, 15,  9,  8,  9, 14,  5,  6,  8,  6,  5, 12,
-       9, 15,  5, 11,  6,  8, 13, 12,  5, 12, 13, 14, 11,  8,  5,  6);
+       9, 15,  5, 11,  6,  8, 13, 12,  5, 12, 13, 14, 11,  8,  5,  6];
 
    -- Rotate Amounts for Right line (SR)
    SR : constant array (Step_Index) of Shift_Amount :=
-     ( 8,  9,  9, 11, 13, 15, 15,  5,  7,  7,  8, 11, 14, 14, 12,  6,
+     [ 8,  9,  9, 11, 13, 15, 15,  5,  7,  7,  8, 11, 14, 14, 12,  6,
        9, 13, 15,  7, 12,  8,  9, 11,  7,  7, 12,  7,  6, 15, 13, 11,
        9,  7, 15, 11,  8,  6,  6, 14, 12, 13,  5, 14, 13, 13,  7,  5,
       15,  5,  8, 11, 14, 14,  6, 14,  6,  9, 12,  9, 12,  5, 15,  8,
-       8,  5, 12,  9, 12,  5, 14,  6,  8, 13,  6,  5, 15, 13, 11, 11);
+       8,  5, 12,  9, 12,  5, 14,  6,  8, 13,  6,  5, 15, 13, 11, 11];
 
    -- Non-linear boolean function for the Left line
    function FL (J : Step_Index; X, Y, Z : Word) return Word is
@@ -110,12 +110,12 @@ package body Ripemd_160 is
          -- Left line processing
          T := AL + FL (J, BL, CL, DL) + X (RL (J)) + KL (J);
          T := Rol (T, SL (J)) + EL;
-         AL := EL; EL := DL; DL := CL; CL := Rol (BL, 10); BL := T;
+         AL := EL; EL := DL; DL := Rol (CL, 10); CL := BL; BL := T;
 
          -- Right line processing
          T := AR + FR (J, BR, CR, DR) + X (RR (J)) + KR (J);
          T := Rol (T, SR (J)) + ER;
-         AR := ER; ER := DR; DR := CR; CR := Rol (BR, 10); BR := T;
+         AR := ER; ER := DR; DR := Rol (CR, 10); CR := BR; BR := T;
       end loop;
 
       -- Combine results back into the hash state
@@ -154,9 +154,9 @@ package body Ripemd_160 is
    procedure Init (Ctx : out Context) is
    begin
       -- Standard RIPEMD-160 initialization constants
-      Ctx.State := (16#67452301#, 16#EFCDAB89#, 16#98BADCFE#,
-                    16#10325476#, 16#C3D2E1F0#);
-      Ctx.Buffer := (others => 0);
+      Ctx.State := [16#67452301#, 16#EFCDAB89#, 16#98BADCFE#,
+                    16#10325476#, 16#C3D2E1F0#];
+      Ctx.Buffer := [others => 0];
       Ctx.Buffer_Len := 0;
       Ctx.Total_Bytes := 0;
       Ctx.Initialized := True;
